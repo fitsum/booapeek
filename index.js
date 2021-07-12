6d948e2b828eb324309961c48b72c43bb822a84d
fitsum <fitsumbelay@gmail.com>
2021-07-12 02:46:48 -0400

    change baseline status and timeout + add EST to goneby line

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const t = require('./utils');
 
 // const p = { screen_name: 'orgX3' };
-const timeout = 60000;
-const goneBy = new Date(new Date().getTime() + timeout).toLocaleTimeString('en', { timeStyle: 'short' });
-const status = `I'm gone by ${goneBy}`;
+const timeout = 90000;
+const goneBy = new Date(new Date().getTime() + timeout).toLocaleTimeString('en', { timeStyle: 'short' }) + ' EST';
+const status = `message gone by ${goneBy}`;
 // t.searchTweets(searchParams)
 // t.getUserFollowers(p);
 // t.getMyTweets(p);
