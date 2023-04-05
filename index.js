e87fb67302628583a743f131c2349c079ccf86b2
fitsum <fitsumbelay@gmail.com>
2023-04-04 21:37:28 -0400

    lint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const t = require('./utils');
+require( 'dotenv' ).config()
+const t = require( './utils' )
 
 // const p = { screen_name: 'orgX3' };
-const timeout = 90000;
-const goneBy = new Date(new Date().getTime() + timeout).toLocaleTimeString('en', { timeStyle: 'short' }) + ' EST';
-const status = `message gone by ${goneBy}`;
+const timeout = 90000
+const goneBy = new Date( new Date().getTime() + timeout ).toLocaleTimeString( 'en', { timeStyle: 'short' } ) + ' EST'
+const status = `message gone by ${ goneBy }`
 // t.searchTweets(searchParams)
 // t.getUserFollowers(p);
 // t.getMyTweets(p);
-t.postMyTweet(status, timeout);
+t.postMyTweet( status, timeout )
--- utils.js
+++ utils.js
@@ -1,19 +1,19 @@
-const Twitter = require('twitter');
+const Twitter = require( 'twitter' )
 
-const client = new Twitter({
+const client = new Twitter( {
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token_key: process.env.ACCESS_TOKEN_KEY,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
-});
+} )
 
 // search tweets and page through results
-const searchTweets = function (params) {
-  client.get('search/tweets', params, function (error, tweets, response) {
-    console.log(tweets.statuses);
-    if (error) {
-      console.log('error:', error);
-      return;
+const searchTweets = function ( params ) {
+  client.get( 'search/tweets', params, function ( error, tweets, response ) {
+    console.log( tweets.statuses )
+    if ( error ) {
+      console.log( 'error:', error )
+      return
     }
     /*
     tweets.statuses.forEach(tweet => {
@@ -26,27 +26,27 @@ const searchTweets = function (params) {
         );
     });
     */
-    const nextResults = tweets.search_metadata.next_results;
-    params.max_id = new URLSearchParams(nextResults).get('max_id');
+    const nextResults = tweets.search_metadata.next_results
+    params.max_id = new URLSearchParams( nextResults ).get( 'max_id' )
     // console.log("params:", params);
-    if (params.max_id !== null) {
-      searchTweets(params);
+    if ( params.max_id !== null ) {
+      searchTweets( params )
     }
-  });
-};
+  } )
+}
 
 // get followers
-const getUserFollowers = function (params) {
-  client.get('followers/list', params, function (error, tweets, response) {
-    if (error) {
-      console.log('error:', error);
-      return;
+const getUserFollowers = function ( params ) {
+  client.get( 'followers/list', params, function ( error, tweets, response ) {
+    if ( error ) {
+      console.log( 'error:', error )
+      return
     }
-    const users = JSON.parse(response.body).users;
-    const nextCursor = JSON.parse(response.body).nextCursor;
-    if (users) {
-      console.log(users);
-      users.forEach((user) => {
+    const users = JSON.parse( response.body ).users
+    const nextCursor = JSON.parse( response.body ).nextCursor
+    if ( users ) {
+      console.log( users )
+      users.forEach( ( user ) => {
         // console.log('user', user)
 
         /*
@@ -60,56 +60,56 @@ const getUserFollowers = function (params) {
                     "\n"
                 );
             */
-      });
-      if (nextCursor !== 0) {
-        params.cursor = nextCursor;
-        getUserFollowers(params);
+      } )
+      if ( nextCursor !== 0 ) {
+        params.cursor = nextCursor
+        getUserFollowers( params )
       }
     }
-  });
-};
+  } )
+}
 
 // get last 7 days of my tweets
-const getMyTweets = function (params) {
-  client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-      const out = tweets;
-      console.log(out);
+const getMyTweets = function ( params ) {
+  client.get( 'statuses/user_timeline', params, function ( error, tweets, response ) {
+    if ( !error ) {
+      const out = tweets
+      console.log( out )
     }
-  });
-};
+  } )
+}
 
 // show tweet data
-const getTweetByID = function (tweetID) {
-  client.get(`statuses/show/${tweetID}`, function (error, tweet, response) {
-    if (!error) {
-      const out = tweet;
-      console.log(out);
+const getTweetByID = function ( tweetID ) {
+  client.get( `statuses/show/${ tweetID }`, function ( error, tweet, response ) {
+    if ( !error ) {
+      const out = tweet
+      console.log( out )
     }
-  });
-};
+  } )
+}
 
 // remove tweet by ID
-const destroyMyTweet = function (id) {
-  client.post(`statuses/destroy/${id}`, function (error, tweet, response) {
-    if (!error) { console.log(`success deleting ID: ${id}`); return; }
-    console.log(error);
-  });
-};
+const destroyMyTweet = function ( id ) {
+  client.post( `statuses/destroy/${ id }`, function ( error, tweet, response ) {
+    if ( !error ) { console.log( `success deleting ID: ${ id }` ); return }
+    console.log( error )
+  } )
+}
 
 // post tweet
-const postMyTweet = function (tweet, halflife) {
-  client.post('statuses/update', { status: `${tweet}` }, function (error, tweet, response) {
-    if (error) { console.log('Failed posting:', error); return; }
-    console.log(`success posting ID: ${tweet.id_str}`);
+const postMyTweet = function ( tweet, halflife ) {
+  client.post( 'statuses/update', { status: `${ tweet }` }, function ( error, tweet, response ) {
+    if ( error ) { console.log( 'Failed posting:', error ); return }
+    console.log( `success posting ID: ${ tweet.id_str }` )
     // if halflife"s defined, delete tweet in ${halflife} ms
-    if (typeof halflife !== 'undefined') {
-      setTimeout(function () {
-        destroyMyTweet(`${tweet.id_str}`);
-      }, halflife);
+    if ( typeof halflife !== 'undefined' ) {
+      setTimeout( function () {
+        destroyMyTweet( `${ tweet.id_str }` )
+      }, halflife )
     }
-  });
-};
+  } )
+}
 
 module.exports = {
   searchTweets,
@@ -117,4 +117,4 @@ module.exports = {
   getMyTweets,
   getTweetByID,
   postMyTweet
-};
+}
