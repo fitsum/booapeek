d386170caebae175b48e84d8cd05931631b6b7a5
fitsum <fitsumbelay@gmail.com>
2023-04-04 22:43:37 -0400

    get status and timeout from args + set default timeout + set format for status + remove unused

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 require( 'dotenv' ).config()
 const t = require( './utils' )
 
-// const p = { screen_name: 'orgX3' };
-const timeout = 90000
+const timeout = ( process.argv[ 3 ] || 30 ) * 1000
 const goneBy = new Date( new Date().getTime() + timeout ).toLocaleTimeString( 'en', { timeStyle: 'short' } ) + ' EST'
-const status = `message gone by ${ goneBy }`
+const status = process.argv[ 2 ] + `\n👋 by ${ goneBy }`
 // t.searchTweets(searchParams)
 // t.getUserFollowers(p);
 // t.getMyTweets(p);
